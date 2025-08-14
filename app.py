8492beb91bdccc1299ca859e6b0f03e59c2c766a
STANISLAV <stassemerikov@gmail.com>
2025-08-14 20:17:18 +0300

    add make_response

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template, url_for
+from wsgiref.util import request_uri
 
-app = Flask(__name__)
+from flask import Flask, render_template, request, make_response, jsonify, url_for
+
+# from logic import * #папка для функций
+
+app = Flask(
+    __name__,
+    # Это можно не прописывать
+    instance_relative_config=False,
+    template_folder="templates",
+    static_folder="static")
 
 
 @app.route('/')  # main page
 @app.route('/main')  # main page
 def index():
     return render_template("index.html")
+
+
+# Это тёмная магия!!!
+@app.route("/give_number", methods=["GET"])
+def get_random_number():
+    if request.method != 'GET':
+        return make_response("request=", 400)
+    headers = {"Content-Type": "aplication/json"}
+    return make_response('It_WORKED!', 200, headers)
 
 
 if __name__ == "__main__":
