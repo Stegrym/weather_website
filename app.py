9c70e2415d59a280d2639e809b9bd933be261b7e
STANISLAV <stassemerikov@gmail.com>
2025-08-18 16:04:42 +0300

    test db_models

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from wsgiref.util import request_uri
-
 from flask import Flask, render_template, request, make_response, jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # from logic import * #папка для функций
+
 
 app = Flask(
     __name__,
@@ -10,10 +11,22 @@ app = Flask(
     instance_relative_config=False,
     template_folder="templates",
     static_folder="static")
+app.config["SQLALCHEMY DATABASE_URI"] = 'sqlite:///city_data_base.db'  # sqlite можно поменять на POSTGRES
+db = SQLAlchemy(app)
 
 
-@app.route('/')  # main page
-@app.route('/main')  # main page
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # --
+    City = db.Column(db.String(50), nollable=False)
+    weather = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<City %r>' % self.id
+
+
+@app.route('/')  # http://localhost:5000/
+@app.route('/main')
 def index():
     return render_template("index.html")
 
