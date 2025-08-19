c7474b92d5c8bb08567c11199de2bae4fec511e9
STANISLAV <stassemerikov@gmail.com>
2025-08-19 15:19:51 +0300

    weather request

---
+++ weather_logic.py
@@ -0,0 +1,19 @@
+import requests
+from config import API_key
+
+
+def get_weather(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': API_key,
+        'units': 'metric',
+        'lang': 'ru',
+    }
+
+    try:
+        response = requests.get(base_url, params=params, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException:
+        return None
