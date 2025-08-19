78d4061d819721beb55c03abfe84a223716dad65
STANISLAV <stassemerikov@gmail.com>
2025-08-19 15:24:35 +0300

    work version

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template, request, make_response, jsonify, url_for
+from flask import Flask, render_template, request
+from weather_logic import get_weather
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-# from logic import * #папка для функций
-
 
 app = Flask(
     __name__,
@@ -12,22 +9,21 @@ app = Flask(
     template_folder="templates",
     static_folder="static")
 
-app.config["SQLALCHEMY DATABASE_URI"] = 'sqlite:///city_data_base.db'  # sqlite можно поменять на POSTGRES
 
-@app.route('/')  # http://localhost:5000/
-@app.route('/main')
+# app.config["SQLALCHEMY DATABASE_URI"] = 'sqlite:///city_data_base.db'  # sqlite можно поменять на POSTGRES
+
+# url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}"
+
+@app.route('/', methods=['GET', 'POST'])  # http://localhost:5000/
+# @app.route('/main')
 def index():
-    return render_template("index.html")
+    weather_data = None
+    if request.method == "POST":
+        city = request.form.get('city')
+        weather_data = get_weather(city)
+    return render_template("index.html", weather=weather_data)
 
 
-# Это тёмная магия!!!
-@app.route("/give_number", methods=["GET"])
-def get_random_number():
-    if request.method != 'GET':
-        return make_response("request=", 400)
-    headers = {"Content-Type": "aplication/json"}
-    return make_response('It_WORKED!', 200, headers)
-
-
+# app.config['DEBUG'] = True
 if __name__ == "__main__":
-    app.run(debug=True)  # False - когда выложу на сервер
+    app.run(debug=True)
