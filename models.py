f5597001d2bac3f22268c45e73e88ebe30989caa
STANISLAV <stassemerikov@gmail.com>
2025-08-26 20:24:51 +0300

    feat: add Users table and password hash

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, Text
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Base(DeclarativeBase):
@@ -31,3 +32,26 @@ class City(db.Model):
             "wind_speed": self.wind_speed,
             "pressure": self.pressure,
         }
+
+
+class Users(db.Model):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(Text, nullable=False)
+
+    def set_password(self, password):
+        """Сохраняем Хэшированный пароль"""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Проверка пароля"""
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        """Вывод данных в виде словаря"""
+
+        return {"id": self.id,
+                "username": self.username,
+                }
